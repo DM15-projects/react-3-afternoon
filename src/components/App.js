dd93dd050d4311eac0c6fe966e03b0f2e40ca978
JonathanWaller <waller.j27@gmail.com>
2018-06-05 16:30:03 -0500

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,78 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
-import './App.css';
+import "./App.css";
 
-import Header from './Header/Header';
-import Compose from './Compose/Compose';
+import Header from "./Header/Header";
+import Compose from "./Compose/Compose";
+import Post from "./Post/Post";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      posts: []
+      posts: [],
+      baseUrl: "https://practiceapi.devmountain.com/api"
     };
 
-    this.updatePost = this.updatePost.bind( this );
-    this.deletePost = this.deletePost.bind( this );
-    this.createPost = this.createPost.bind( this );
+    this.updatePost = this.updatePost.bind(this);
+    this.deletePost = this.deletePost.bind(this);
+    this.createPost = this.createPost.bind(this);
   }
-  
+
   componentDidMount() {
-
+    axios
+      .get(`${this.state.baseUrl}/posts`)
+      .then(response => {
+        console.log(response.data);
+        this.setState({ posts: response.data });
+      })
+      .catch(() => console.log("Sorry, you broke it"));
   }
 
-  updatePost() {
-  
+  updatePost(id, text) {
+    axios.put(`${this.state.baseUrl}/posts?id=${id}, {text}`).then(response => {
+      // console.log(response.data);
+      this.setState({ posts: response.data });
+    });
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios.delete(`${this.state.baseUrl}/posts?id=${id}`).then(response => {
+      console.log(response);
+      this.setState({ posts: response.data });
+    });
   }
 
-  createPost() {
-
+  createPost(text) {
+    axios.post(`${this.state.baseUrl}/posts`, { text }).then(response => {
+      console.log(response);
+      this.setState({ posts: response.data });
+    });
   }
 
   render() {
     const { posts } = this.state;
+    let myPosts = posts.map(elem => (
+      <Post
+        key={elem.id}
+        text={elem.text}
+        date={elem.date}
+        updatePostFn={this.updatePost}
+        id={elem.id}
+        deletePostFn={this.deletePost}
+      />
+    ));
 
     return (
       <div className="App__parent">
         <Header />
 
         <section className="App__content">
-
-          <Compose />
-          
+          <Compose createPostFn={this.createPost} />
+          {myPosts}
+          {/* {posts.map(elem => <Post key={elem.id} />)} */}
         </section>
       </div>
     );
